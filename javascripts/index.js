cc8635e6594cca99e4314b6fe8c6b749b75c0022
Pablo Lluch <pablo.lluch@gmail.com>
2014-06-04 02:24:49 -0400

    Update project

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,50 @@
 
-$(function() {
-	console.log('Success');
-});
+Backbone.sync = function(method, model, options) {
+
+	var methodMap = {
+		'create' : 'POST',
+		'read' : 'GET',
+		'update' : 'PUT',
+		'delete' : 'DELETE'
+	};
+
+	// console.log(method);
+	// console.log(model);
+	// console.log(options);
+
+	switch(method) {
+		case 'create': 
+		break;
+
+		case 'read':
+			if(model.idAttribute) {
+				var id = model[model.idAttribute];
+				var url = model.urlRoot + '/' + id;
+				$.getJSON(url)
+				.done(function(data, statusText, xhr) {
+					options.success(data);
+					model.trigger("sync");
+				})
+				.fail(function(xhr, statusText, responseJSON) {
+					// console.log(xhr);
+					// console.log(statusText);
+					// console.log(responseJSON);
+					options.error(responseJSON);
+					model.trigger("error");
+				});
+			}
+		break;
+
+		case 'update':
+		break;
+
+		case 'delete':
+		break;
+	}
+
+};
+
+
 
 var Complaint = Backbone.Model.extend({
 	
@@ -31,15 +74,35 @@ complaints.on('change', function(c) {
 	console.log('Updated');
 });
 
-var c;
-function update() {
+var c = new Complaint({
+	id: 456
+});
 
-	complaints.fetch({
-		success: function(model, response) {
-			c = complaints.get(19);
-		}
-	});
-}
+var model, response, options;
+
+c.fetch({
+	success: function(model, response, options) {
+		console.log(model);
+		console.log(response);
+		console.log(options);
+	},
+	error: function(model, xhr, options) {
+		console.log(model);
+		console.log(xhr);
+		console.log(options);
+	}
+});
+// function update() {
+
+// 	complaints.fetch({
+// 		success: function(model, response) {
+// 			c = complaints.get(19);
+// 		},
+// 		add: true,
+// 		change: true,
+// 		remove: true
+// 	});
+// }
 
 
-update();
+// update();
